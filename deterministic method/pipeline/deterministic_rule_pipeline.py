15e899b96b2ed083ed5cbbc5be88abbe6cbf07c9
Xi Yang <yunxiache@gmail.com>
2017-07-07 01:45:20 -0400

    add rules

--- deterministic method/pipeline/deterministic_rule_pipeline.py
+++ deterministic method/pipeline/deterministic_rule_pipeline.py
@@ -10,12 +10,29 @@ title = ['ENTERPRISEID','LAST_','FIRST_','MIDDLE','SUFFIX_','DOB','GENDER','SSN'
 def sql(rule):
 	with engine.begin() as conn:
 		res = conn.execute(rule)
+	for each in res:
+		print(each)
+	return res
 
+def get_rules():
+	d = dict()
+	with open('rules.txt', 'r') as f:
+		for line in f:
+			rule = line[:-1].split("@")
+			rule_title = rule[0]
+			rule_sql = rule[1]
+			d[rule_title] = rule_sql 
+	return d
+
+def rule2csv(file, data):
+	with open(file, "w") as f:
+		writer = csv.writer(f)
 
 
 def main(): 
-	# with open('temp\\1.txt', 'w') as f:
-	# 	print("hello", file = f)
-
+	rules = get_rules()
+	for each in rules:
+		sql(rules[each])
+			
 if __name__ == '__main__':
 	main()
