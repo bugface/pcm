35052c0cb298d9db14e0f47babf4e9720542b885
alex <alexgre@ufl.edu>
2017-07-08 01:57:12 -0400

    update exp7 and 8

--- deterministic method/pipeline/deterministic_rule_pipeline.py
+++ deterministic method/pipeline/deterministic_rule_pipeline.py
@@ -1,38 +1,145 @@
 from sqlalchemy import create_engine
 import csv
-import os
-import sys
+import logging
+import glob
+
+FORMAT = '%(asctime)-20s %(name)-5s %(levelname)-10s %(message)s'
+logging.basicConfig(filename='rules.log',level=logging.INFO, format=FORMAT, datefmt="%Y-%m-%d %H:%M:%S")
+logger = logging.getLogger("task")
+
 SQLALCHEMY_DATABASE_URI = "oracle://alexgre:alex1988@temp1.clx2hx01phun.us-east-1.rds.amazonaws.com/ORCL"
 engine = create_engine(SQLALCHEMY_DATABASE_URI)
-title = ['ENTERPRISEID','LAST_','FIRST_','MIDDLE','SUFFIX_','DOB','GENDER','SSN','ADDRESS1','ADDRESS2', 
-			'ZIP','MOTHERS_MAIDEN_NAME','MRN','CITY','STATE_','PHONE','PHONE2','EMAIL','ALIAS_']
+title = ['ENTERPRISEID','LAST_','FIRST_','MIDDLE','SUFFIX_','DOB','GENDER','SSN','ADDRESS1','ADDRESS2', 'ZIP','MOTHERS_MAIDEN_NAME','MRN','CITY','STATE_','PHONE','PHONE2','EMAIL','ALIAS_']
+
+def create_submission_csv(file, csv_file):
+	with open(file, "r") as f:
+		with open(csv_file, "w", newline='') as f1:
+			writer = csv.writer(f1)
+			for each in f:
+				l = []
+				data = each[:-1].split('\t')
+				l.append(data[0])
+				l.append(data[1])
+				l.append(1)
+				writer.writerow(l)
 
 def sql(rule):
 	with engine.begin() as conn:
 		res = conn.execute(rule)
-	for each in res:
-		print(each)
 	return res
 
-def get_rules():
+def get_rules(file):
 	d = dict()
-	with open('rules.txt', 'r') as f:
+	with open(file, 'r') as f:
 		for line in f:
 			rule = line[:-1].split("@")
 			rule_title = rule[0]
 			rule_sql = rule[1]
-			d[rule_title] = rule_sql 
+			d[rule_title] = rule_sql
 	return d
 
-def rule2csv(file, data):
+def pair2txt(file, data):
 	with open(file, "w") as f:
+		for each in data:
+			output = "{}\t{}".format(each[0], each[1])
+			print(output, file=f, end='\n')
+
+def store_pairs(rules):
+	for rule in rules:
+		file = "txt\\" + rule + ".txt"
+		res = sql(rules[rule])
+		data = []
+		s = set()
+		for each_res in res:
+			t = (each_res[0], each_res[1])
+			tprim = (each_res[1], each_res[0])
+			if t not in s and tprim not in s:
+				s.add(t)
+				data.append(t)
+		pair2txt(file, data)
+		logger.info("rule: {}; pairs number: {}".format(file, len(data)))
+		res.close()
+
+def _dedupe(file, dataset):
+	with open(file, "r") as f:
+		for each in f:
+			data = each[:-1].split('\t')
+			t = (data[0], data[1])
+			tprim = (data[1], data[0])
+			if t not in dataset and tprim not in dataset:
+				dataset.add(t)
+
+def combine(base, files):
+	pairs = set()
+	_dedupe(base, pairs)
+
+	for file in files:
+		_dedupe(file, pairs)
+
+	with open("combined.txt", "w") as f:
+		for each in pairs:
+			output = "{}\t{}".format(each[0], each[1])
+			print(output, file = f, end='\n')
+
+def diff(base, files):
+	pairs = set()
+	diff_pairs = set()
+	_dedupe(base, pairs)
+
+	for file in files:
+		with open(file, "r") as f:
+			for each in f:
+				data = each[:-1].split('\t')
+				t = (data[0], data[1])
+				tprim = (data[1], data[0])
+				if t not in pairs and tprim not in pairs:
+					pairs.add(t)
+					diff_pairs.add(t)
+	return diff_pairs
+
+def pairs2csv(pairs, file):
+	title.insert(0, "index")
+	with open(file, "w", newline='') as f:
 		writer = csv.writer(f)
+		writer.writerow(title)
+		title.pop(0)
+		for i, pair in enumerate(pairs):
+			l1 = [i]
+			l2 = [i]
+			pair1 = int(pair[0])
+			pair2 = int(pair[1])
+			sql1 = "select * from pcm where enterpriseid = {}".format(pair1)
+			sql2 = "select * from pcm where enterpriseid = {}".format(pair2)
+			with engine.begin() as conn:
+				res1 = conn.execute(sql1).fetchone()
+				for each in title:
+					l1.append(res1[each.lower()])
+				writer.writerow(l1)
+				res2 = conn.execute(sql2).fetchone()
+				for each in title:
+					l2.append(res2[each.lower()])
+				writer.writerow(l2)
+
+def pairs2txt(data, file):
+	with open(file, "w") as f:
+		for each in data:
+			output = "{}\t{}".format(each[0], each[1])
+			print(output, file=f, end='\n')
+
+def main():
+	# rules = get_rules('rules_pair.txt')
+	# store_pairs(rules)
+	txt_files = glob.glob("txt\*.txt")
+	base_file = "union_stgy4.txt"
+	# combine(base_file, txt_files)
+	# create_submission_csv('combined.txt', 'sub7.csv')
+	# dp_set = diff(base_file, txt_files)
+	# pairs2txt(dp_set, 'diffpairs.txt')
+	# create_submission_csv('diffpairs.txt', 'sub8.csv')
+	# pairs2csv(dp_set, "rulesdiff.csv")
 
 
-def main(): 
-	rules = get_rules()
-	for each in rules:
-		sql(rules[each])
-			
+
+
 if __name__ == '__main__':
 	main()
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 ## dedupe
 1. use csvdedupe_modified.py obtained a submission result as submission1 -> precision=0.413 recall=0.3
-2. seperate 3 records into two or three pairs -> obtain submission2 -> precision=0.84 recall 0.65 
+2. seperate 3 records into two or three pairs -> obtain submission2 -> precision=0.84 recall 0.65
 <br><br>
 #### *************************************************************************************************************
 
@@ -8,7 +8,7 @@
 ## Deterministic Experiments:
 1. 	- strategy: query db for same f+l+dob+ssn+gender
 	- txt files: f_l_dob_ssn_g.txt (total: 10950)
-	- submission file: per1.csv 
+	- submission file: per1.csv
 	- result: precision=1 and recall=0.19
 <br><br>
 2.	- strategy: query db for same f+l+dob+gender
@@ -24,7 +24,7 @@
 4.  - strategy: union the dataset(f_l_dob_g.txt) from stgy2 and the dataset(union_stgy3.txt) from stgy3
 	- txt files: union_stgy4.txt(total: 38635)
 	- submission: sub3.csv
-	- result: precision=0.9997 and recall=0.67
+	- result: precision=0.9997 and recall=0.67 (12 error pairs)
 <br><br>
 5.  - strategy: query db for same f+l+address1 and union with dataset(union_stgy4.txt) from stgy4
 	- txt files: union_stgy5.txt (total: 40281)
@@ -33,28 +33,29 @@
 <br><br>
 6.  - strategy: query db for same f+l
 	- txt files: f_l.txt (total: 740281)
-	- submission: sub6.csv
-	- result: precision=0.06 and recall=0.736
+<br><br>
+7.  - strategy: query db using rules combined with exp5
+	- txt files: combined.txt (total: 50668)
+	- submission: sub7.csv
+	- result: precision=0.953 and recall=0.84
+<br><br>
+8.  - strategy: query db using all rules, filter all the distinct pairs not contained in experiment 4
+	- txt files: combined.txt (total: 9853)
+	- submission: sub8.csv
+	- result: precision=0.984 and recall=0.17 (has around 150 error pairs, will be checked by hand in diffpair.csv)
 <br><br>
 #### *************************************************************************************************************
 
 ## table of fields combination
-
-### table (A) of combination 3
-
-| index   | combination   | pairs   | test?(y/n)   | percision   | recall   |
-|---|---|---|---|---|---|
-| 1   | f+l+addr  | 12460   | n  |   |   |
-
-### table (B) of combination 4 or 5
 
 | index   | combination   | pairs   | test?(y/n)   | percision   | recall   |
 |---|---|---|---|---|---|
 | 1  | f+l+dob+ssn+gender  | 10950  | y | 1  | 0.19  |
 | 2  | f+l+dob+ssn  | 13960  | n  |   |   |
 | 3  | f+l+gender+ssn  | 13235  | n |   |   |
-| 4  | f+l+dob+gender  | 33474  | y | 0.997  | 0.58  |
+| 4  | f+l+dob+gender  | 33474  | y | 0.9997  | 0.58  |
 | 5  | f+dob+gender(F)+ssn  | 2882  | n |   |   |
+| 6  | f+l+addr  | 12460   | n  |   |   |
 
 
 
